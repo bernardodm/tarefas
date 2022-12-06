efcab12e2c3ed4d1b7bd892a75d0e4e5b0112c9c
Bernardo Duarte de Miranda <112958256+bernardodm@users.noreply.github.com>
2022-12-06 15:02:47 -0300

    atualização lista

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+let contador = 0
 let input = document.getElementById('itexto');
 let btnadd = document.getElementById('iadd');
 let main = document.getElementById('lista');
@@ -6,23 +7,55 @@ function addTarefa() {
     let valorInput = input.value;
 
     if ((valorInput !=="") && (valorInput !==null) && (valorInput !==undefined)) {
+        ++contador;
+
         let novoItem = 
-    `<div class="item">
-        <div class="item-icone">
-            <span class="material-symbols-outlined">
+    `<div id="${contador}" class="item">
+        <div onclick="marcarTarefa(${contador})" class="item-icone">
+            <span id="icone_${contador}" class="material-symbols-outlined">
                 radio_button_unchecked
                 </span>
         </div>
-        <div class="item-nome">
+        <div onclick="marcarTarefa(${contador})" class="item-nome">
             ${valorInput}
         </div>
         <div class="item-botao">
-            <button class="deletar"><span class="material-symbols-outlined">delete_forever</span>
+            <button onclick="deletar(${contador})" class="deletar"><span class="material-symbols-outlined">delete_forever</span>
                 Deletar
             </button>
         </div>
     </div>`;
 
     main.innerHTML += novoItem;
+
+    input.value= "";
+    input.focus();
     }
 }
+
+function deletar(id) {
+    var tarefa = document.getElementById(id);
+    tarefa.remove();
+}
+
+function marcarTarefa(id) {
+    var item = document.getElementById(id);
+    var classe = item.getAttribute('class');
+
+    if (classe=="item") {
+        item.classList.add('clicado');
+        var icone = document.getElementById('icone_'+id);
+        item.parentNode.appendChild(item);
+    }else {
+        item.classList.remove('clicado');
+        var icone = document.getElementById('icone_'+id);
+    }
+}
+
+input.addEventListener("keyup", function(event){
+    // Se teclou Enter (tecla 13)
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        btnadd.click();
+    }
+})
